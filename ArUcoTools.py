2a8388fa4fc936961b8e1f750bc7964ade7bb6c0
Tershire <84491744+Tershire@users.noreply.github.com>
2023-03-10 17:33:23 +0100

    Update ArUcoTools.py

--- ArUcoTools.py
+++ ArUcoTools.py
@@ -6,7 +6,8 @@ class for ArUco Markers
 1st written on: 2023 MAR 09
 guided by: https://python-academia.com/en/opencv-aruco/
            https://stackoverflow.com/questions/74964527/attributeerror-module-cv2-aruco-has-no-attribute-dictionary-get
-
+           https://github.com/GSNCodes/ArUCo-Markers-Pose-Estimation-Generation-Python/blob/main/pose_estimation.py
+           https://gist.github.com/edward1986/ed1ae7f8a10a3d44dc82360a38c4c6b9
 """
 
 # IMPORT //////////////////////////////////////////////////////////////////////
@@ -14,7 +15,7 @@ import cv2 as cv
 from cv2 import aruco
 import numpy as np
 import time
-import vision_tools as vt
+import threading
 
 
 # SETTING /////////////////////////////////////////////////////////////////////
@@ -22,15 +23,18 @@ import vision_tools as vt
 
 # CLASS ///////////////////////////////////////////////////////////////////////
 class DetectMarker:
+    """
+    continuously detect ArUco Marker viewed on camera
+    """
     dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_250)
     parameters = aruco.DetectorParameters()
 
     def __init__(self, camera_id):
         self.cap = cv.VideoCapture(camera_id)
 
-    def get_marker_id(self):
+    def get_marker_info(self):
         """
-        get marker id list from an image
+        get marker id list
         :return:
         """
         ret, frame = self.cap.read()
@@ -40,10 +44,47 @@ class DetectMarker:
         corners, ids, rejected_img_points = detector.detectMarkers(gray)
         ids = np.ravel(ids)
 
-        return ids, self.cap
+        return corners, ids, self.cap
+
+class ArUcoStreamer(threading.Thread):
+    """
+    "Killable Thread" to show live stream
+    """
+    def __init__(self, cap):
+        super(ArUcoStreamer, self).__init__()
+        self._stop_event = threading.Event()
+        self.cap = cap
+
+    def stop(self):
+        self._stop_event.set()
+
+    def stopped(self):
+        return self._stop_event.is_set()
+
+    def run(self):
+        self.stream_video(self.cap)
+
+    def stream_video(self, cap):
+        # stream a video
+        while True:
+            # get ArUco info
+            corners, ids, _ = detect_marker_cam0.get_marker_info()
+
+            ret, frame = cap.read()
+            aruco.drawDetectedMarkers(frame, corners)
+            print("get_marker_id_cam0")
+            print(ids)
+            # aruco.drawAxis(frame, )
+            cv.imshow('View', frame)
+            cv.waitKey(1)
+
+            if self.stopped():
+                cap.release()
+                cv.destroyAllWindows()
+                break
 
 
-# Test
+# TEST ////////////////////////////////////////////////////////////////////////
 if __name__ == "__main__":
     dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_250)
     parameters = aruco.DetectorParameters()
@@ -52,21 +93,18 @@ if __name__ == "__main__":
     camera_id = 0
     detect_marker_cam0 = DetectMarker(camera_id)
 
-    _, cap = detect_marker_cam0.get_marker_id()
-    streamer = vt.CapStreamer(cap)
+    _, _, cap = detect_marker_cam0.get_marker_info()    # what is better?
+    streamer = ArUcoStreamer(cap)
 
     # start Thread
     streamer.start()
 
-    try:
-        while True:
-            print("get_marker_id_cam0")
-            ids, _ = detect_marker_cam0.get_marker_id()
-            print(ids)
-            time.sleep(1)
-
+    """
     except KeyboardInterrupt:
         detect_marker_cam0.cap.release()
+    """
+
+    input("Press Any Key to Stop")    # how can I wait until user do something?
 
     # finish Thread
     streamer.stop()
